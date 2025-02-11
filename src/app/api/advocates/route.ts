2f0bc325f699074d08e96f763c33c186bdf0d4ec
Brandon Roberts <brobertstech@gmail.com>
2025-02-11 10:00:44 -0800

    Use database to fetch advocates
    Uncommented the .env line for the database connection.

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,12 +1,27 @@
+import { NextRequest, NextResponse } from "next/server";
+
 import db from "../../../db";
 import { advocates } from "../../../db/schema";
-import { advocateData } from "../../../db/seed/advocates";
 
+/**
+ * Fetches a list of Advocate
+ * @returns {Promise<NextResponse<{ data: Advocate[] }>>}
+ */
 export async function GET() {
-  // Uncomment this line to use a database
-  // const data = await db.select().from(advocates);
-
-  const data = advocateData;
-
-  return Response.json({ data });
+  try {
+    const data = await db.select().from(advocates);
+    return NextResponse.json({ data }, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching /app/api/advocates", error);
+    /**
+     * Pretend I send something to an error logging service here
+     * ex:
+     *
+     * Sentry.captureException(error)
+     */
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }
